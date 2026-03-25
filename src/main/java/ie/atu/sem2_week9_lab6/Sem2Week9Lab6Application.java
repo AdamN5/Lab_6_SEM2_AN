19208b44e686a8f539a43efd69127c2f30f95bd0
Adam Nafea <g00462155@atu.ie>
2026-03-25 13:32:15 +0000

    Added files to start

---
+++ src/main/java/ie/atu/sem2_week9_lab6/Sem2Week9Lab6Application.java
@@ -0,0 +1,13 @@
+package ie.atu.sem2_week9_lab6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sem2Week9Lab6Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sem2Week9Lab6Application.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/sem2_week9_lab6/Sem2Week9Lab6ApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.sem2_week9_lab6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sem2Week9Lab6ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
